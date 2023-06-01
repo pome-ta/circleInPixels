4671345802be5bda4c5d464a9e9a8c1ade9370da
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-01 15:56:43 +0900

    up

---
+++ sandbox/230601_1542.py
@@ -0,0 +1,39 @@
+import ui
+
+
+class View(ui.View):
+
+  def __init__(self):
+    self.bg_color = 1
+
+  def did_load(self):
+    pass
+
+  def will_close(self):
+    pass
+
+  def draw(self):
+    _, _, w, h = self.frame
+    r = 9
+    n = int(r * 2)
+    l = min(w, h)
+    cell_size = l / n
+
+    for x in range(n):
+      for y in range(n):
+        ui.set_color(0.25)
+        rect = ui.Path.rect(cell_size * x, cell_size * y, cell_size, cell_size)
+        rect.fill()
+        ui.set_color(0.75)
+        rect.stroke()
+
+  def layout(self):
+    pass
+
+
+if __name__ == '__main__':
+  view = View()
+  #view.present()
+  #view.present(hide_title_bar=True)
+  view.present(style='fullscreen', orientations=['portrait'])
+
