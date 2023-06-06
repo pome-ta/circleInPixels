42d3c04a388d097f44c5c98183111e429977deb9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-06 14:03:40 +0900

    oval

--- sandbox/230605_2326.py
+++ sandbox/230605_2326.py
@@ -1,4 +1,5 @@
 # 円を作る
+import math
 import ui
 
 
@@ -9,9 +10,27 @@ class View(ui.View):
 
   def draw(self):
     _, _, w, h = self.frame
+    back_size = min(w, h) / 1.25
+    x_pos = (w / 2) - (back_size / 2)
+    y_pos = (h / 2) - (back_size / 2)
     ui.set_color(0.25)
-    back = ui.Path.rect(w / 4, h / 4, w / 2, h / 2)
+    back = ui.Path.rect(x_pos, y_pos, back_size, back_size)
     back.fill()
+
+    ui.set_color('red')
+
+    r = back_size / 2
+
+    ov = 5
+    cnt_x = x_pos + (back_size / 2) - (ov / 2)
+    cnt_y = y_pos + (back_size / 2) - (ov / 2)
+
+    for i in range(0, 360, 5):
+      rad = math.radians(i)
+      x_r = cnt_x + (r * math.sin(rad))
+      y_r = cnt_y + (r * math.cos(rad))
+      oval = ui.Path.oval(x_r, y_r, ov, ov)
+      oval.fill()
 
   def layout(self):
     pass
