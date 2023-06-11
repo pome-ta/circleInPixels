6eb118bfbe5c41a734892f9780161d2ea1110b98
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-11 17:44:40 +0900

    up

--- sandbox/230611_1610.py
+++ sandbox/230611_1610.py
@@ -6,28 +6,53 @@ import ui
 class View(ui.View):
 
   def __init__(self, r: int = 2):
-    # todo: 初期定義
+    # --- 初期定義
     self.cell_rad: int
     self.cell_dia: int
     self.cell_size: float
     self.grid_size: float
     self.cells: list
 
+    # --- color 定義
+    self.g_stroke = 0.75
+    self.g_fill = 0.25
+    self.c1 = 'red'
+
+    # --- 変数反映
     self.cell_rad = r
     self.bg_color = 1
 
+  def init_grid_colors(self):
+    # --- 中心線塗り
+    is_center = lambda _x, _y: True if _x == self.cell_rad or _y == self.cell_rad else False
+
+    for x, rows in enumerate(self.cells):
+      for y, cell in enumerate(rows):
+        ui.set_color(self.c1) if is_center(x, y) else ui.set_color(self.g_fill)
+        cell.fill()
+        ui.set_color(self.g_stroke)
+        cell.stroke()
+
+  def setup_grid_cells(self, width: float, height: float):
+    gs = min(width, height)
+    cd = int(self.cell_rad * 2) + 1
+    cs = gs / cd
+
+    rd = range(cd)
+    # xxx: `rect` 生成と位置サイズ指定を関数化（お行儀良くないけど、長くなるので）
+    create_rect = lambda _x, _y: ui.Path.rect(cs * _x, cs * _y, cs, cs)
+
+    self.cells = [[create_rect(x, y) for y in rd] for x in rd]
+
+    self.grid_size = gs
+    self.cell_dia = cd
+    self.cell_size = cs
+
   def draw(self):
+    # todo: view 確定後に、画面位置サイズ情報を取得
     _, _, w, h = self.frame
-    self.cell_dia = int(self.cell_rad * 2) + 1
-    self.grid_size = min(w, h)
-    self.cell_size = self.grid_size / self.cell_dia
-
-    dia_range = range(self.cell_dia)
-
-    create_rect = lambda _x, _y: ui.Path.rect(
-      self.cell_size * _x, self.cell_size * _y, self.cell_size, self.cell_size)
-
-    self.cells = [[create_rect(x, y) for y in dia_range] for x in dia_range]
+    self.setup_grid_cells(w, h)
+    self.init_grid_colors()
 
   def layout(self):
     pass
