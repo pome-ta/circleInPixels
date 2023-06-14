cd80366b609c9173ac813a27f001df5737a12b6d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-14 20:05:26 +0900

    up

---
+++ sandbox/230614_1948.py
@@ -0,0 +1,96 @@
+# 中心から、セルごとに線を引く
+import math
+import ui
+
+
+class View(ui.View):
+
+  def __init__(self, r: int = 2):
+    # --- 初期定義
+    self.cell_rad: int
+    self.cell_dia: int
+    self.cell_size: float
+    self.grid_size: float
+    self.cells: list
+    self.indexs: list
+
+    # --- color 定義
+    self.g_stroke: str | float | int = 0.75
+    self.g_fill: str | float | int = 0.25
+    self.c1: str | float = 'red'
+    self.c2: str | float = 'blue'
+
+    # --- 変数反映
+    self.cell_rad = r
+    self.bg_color = 1
+
+  def init_grid_colors(self):
+    # --- 中心線塗り
+    _r = self.cell_rad
+    is_center = lambda _x, _y: True if _x == _r or _y == _r else False
+
+    for x, rows in enumerate(self.cells):
+      for y, cell in enumerate(rows):
+        ui.set_color(self.c1) if is_center(x, y) else ui.set_color(self.g_fill)
+        cell.fill()
+        ui.set_color(self.g_stroke)
+        cell.stroke()
+
+  def setup_grid_cells(self, width: float, height: float):
+    gs = min(width, height)
+    cd = int(self.cell_rad * 2) + 1
+    cs = gs / cd
+
+    rd = range(cd)
+
+    self.indexs = [[x, y] for y in range(cd) for x in range(cd)]
+    # xxx: `rect` 生成と位置サイズ指定を関数化（お行儀良くないけど、長くなるので）
+    create_rect = lambda _x, _y: ui.Path.rect(cs * _x, cs * _y, cs, cs)
+
+    # xxx: `ui.Path` オブジェクトと、そのインデックス情報の二つを渡したい
+    # xxx: インデックス情報を今後回した方が、処理早いかも？と思ってるけど、そんなこともないかな？
+    self.cells = [[create_rect(x, y) for y in rd] for x in rd]
+    #self.cells = [create_rect(x, y) for x, y in self.indexs]
+
+    #self.cells = [[create_rect(x, y) for y in rd] for x in rd]
+
+    self.grid_size = gs
+    self.cell_dia = cd
+    self.cell_size = cs
+
+  def _c2g(self, index: int) -> float:
+    size_len = index * self.cell_size
+    return size_len - (self.cell_size / 2) if size_len else size_len
+
+  def cell_to_line(self, s: list[int], e: list[int], length: float):
+    line = ui.Path()
+
+  def draw(self):
+    # todo: view 確定後に、画面位置サイズ情報を取得
+    _, _, w, h = self.frame
+    self.setup_grid_cells(w, h)
+    self.init_grid_colors()
+
+    for i in range(self.cell_rad):
+      #print(i)
+      i
+    line = ui.Path()
+    line.line_width = 1
+    line.move_to(w / 2, self.cell_size * self.cell_rad)
+    line.line_to(w, self.cell_size * self.cell_rad)
+    ui.set_color(self.c2)
+    line.stroke()
+    #print(self.cell_size * self.cell_dia)
+    print(self._c2g(1))
+
+  def layout(self):
+    pass
+
+
+if __name__ == '__main__':
+  cell_radius: int = 8
+  view = View(cell_radius)
+  #view.present()
+  #view.present(hide_title_bar=True)
+  view.present(style='fullscreen', orientations=['portrait'])
+
