b8d2deb5dc4f6262cd52b13dfc2ea227d894ae00
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-22 20:40:16 +0900

    line

---
+++ sandbox/230622_1504.py
@@ -0,0 +1,93 @@
+# 中心から、セルごとに線を引く
+import math
+import colorsys
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+
+import ui
+
+# --- color 定義
+g_stroke: str | float | int = 0.75
+g_fill: str | float | int = 0.25
+c0: str | float = 0.88
+c1: str | float = 'maroon'
+c2: str | float = 'blue'
+c3: str | float = 'green'
+c4: str | float = 'yellow'
+
+
+def round_halfup(f: float) -> int:
+  return Decimal(str(f)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
+
+
+class View(ui.View):
+
+  def __init__(self, r: int = 2):
+    # --- 初期定義
+    self.cell_rad: int  # cell の半径
+    self.cell_dia: int  # cell の全径
+    self.cell_size: float
+    self.grid_size: float
+    self.cells: list[list[ui.Path]]
+    self.rect_edge_index: list[list[int]] = []
+
+    # --- 変数反映
+    self.bg_color = 1
+    self.cell_rad = r
+
+  def set_cell_color(self,
+                     cell: ui.Path,
+                     fill: str | float,
+                     stroke: str | float | None = None):
+    stroke = fill if stroke == None else stroke
+    ui.set_color(fill)
+    cell.fill()
+    ui.set_color(stroke)
+    cell.stroke()
+
+  def init_grid_colors(self):
+    # --- 中心線塗り
+    _r = self.cell_rad
+    is_center = lambda _x, _y: True if _x == _r or _y == _r else False
+    [[
+      self.set_cell_color(cell, c1 if is_center(x, y) else g_fill, g_stroke)
+      for y, cell in enumerate(rows)
+    ] for x, rows in enumerate(self.cells)]
+
+  def setup_grid_cells(self, width: float, height: float):
+    gs = min(width, height)
+    cd = int(self.cell_rad * 2) + 1
+    cs = gs / cd
+
+    create_rect = lambda _x, _y: ui.Path.rect(cs * _x, cs * _y, cs, cs)
+
+    self.cells = [[create_rect(x, y) for y in range(cd)] for x in range(cd)]
+
+    self.grid_size = gs
+    self.cell_dia = cd
+    self.cell_size = cs
+
+  def setup_rect_edge_index(self):
+    for x in range(self.cell_dia):
+      for y in range(self.cell_dia):
+        if (0 < y < self.cell_dia - 1) and (0 < x < self.cell_dia - 1):
+          continue
+        self.rect_edge_index.append([x, y])
+        cell = self.cells[x][y]
+        self.set_cell_color(cell, c2, g_stroke)
+
+  def draw(self):
+    # todo: view 確定後に、画面位置サイズ情報を取得
+    _, _, w, h = self.frame
+    self.setup_grid_cells(w, h)
+    self.init_grid_colors()
+    self.setup_rect_edge_index()
+
+  def layout(self):
+    pass
+
+
+if __name__ == '__main__':
+  cell_radius: int = 4
+  view = View(cell_radius)
+  view.present(style='fullscreen', orientations=['portrait'])
+
