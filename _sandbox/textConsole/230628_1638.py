901ae440991915be02962b05c89fc8e8801c33a6
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-28 16:57:07 +0900

    uo

---
+++ _sandbox/textConsole/230628_1638.py
@@ -0,0 +1,8 @@
+# [計算機科学の分野で、直感に反する法則などはありますか? - Quora](https://jp.quora.com/%E8%A8%88%E7%AE%97%E6%A9%9F%E7%A7%91%E5%AD%A6%E3%81%AE%E5%88%86%E9%87%8E%E3%81%A7-%E7%9B%B4%E6%84%9F%E3%81%AB%E5%8F%8D%E3%81%99%E3%82%8B%E6%B3%95%E5%89%87%E3%81%AA%E3%81%A9%E3%81%AF%E3%81%82%E3%82%8A%E3%81%BE)
+
+radius_length: int = 5
+
+indexs = [[[x, y] for y in range(radius_length)] for x in range(radius_length)]
+
+cells = [[0 for column in rows] for rows in indexs]
+
