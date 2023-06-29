467eec37375691df0318430873dd0a499fe31514
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-06-29 15:26:48 +0900

    up

--- _sandbox/textConsole/230629_1229.py
+++ _sandbox/textConsole/230629_1229.py
@@ -1,10 +1,15 @@
 r = 5
 
 pick_list = []
-
+l = (r * 2) + 1
 a = r
 x = 0
 y = r
+
+
+def set_indexs(a, b):
+  return
+
 
 while x < y:
   x += 1
@@ -12,5 +17,14 @@ while x < y:
   if a < 0:
     y -= y
     a = a + (y * 2) - 1
-  pick_list.append([[x, y], [-x, y], [x, -y], [y, x], [-y, -x], y, -x])
+  pick_list.append([
+    [x, y],
+    [-x, y],
+    [-x, -y],
+    [x, -y],
+    [y, x],
+    [-y, x],
+    [-y, -x],
+    [y, -x],
+  ])
 
